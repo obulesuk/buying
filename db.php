c1cf4339d09b58628b77ebf7724cae5bae4061d4
obulesu <obulesu.kuraku@gmail.com>
2016-01-13 01:40:35 +0530

    Update db.php

--- db.php
+++ db.php
@@ -3,7 +3,10 @@
 $dbhost = 'localhost';
    $dbuser = 'root';
    $dbpass = '';
+   // mysql connection
    
-   $con = mysql_connect($dbhost, $dbuser, $dbpass);
+   $con = mysql_connect($dbhost, $dbuser, $dbpass); 
+   
+   // db connection
    mysql_select_db("pavan");
 ?>
